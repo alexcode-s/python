dea02b309fef3a5f8e3bf6e2920c876f07bd76be
alexcode-s <sergioafc17@gmail.com>
2026-05-25 09:52:28 +0200

    Cambios

--- training/extras/ej1.py
+++ training/extras/ej1.py
@@ -7,7 +7,7 @@ intentos = 0
 while not todos_iguales:
     dados = [random.randint(1, 6) for _ in range(n_dados)]
     intentos += 1
-    print(" – ".join(str(d) for d in dados))
+    print(*dados, sep="-")
     todos_iguales = len(set(dados)) == 1
 
 print(f"He tenido que lanzar los dados {intentos} veces para que todos sean iguales")
